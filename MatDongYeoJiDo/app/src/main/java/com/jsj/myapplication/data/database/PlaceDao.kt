b437078f89fae008b2f1fd846f0d643a31fcf04d
Yeon dong <jae073@naver.com>
2024-11-28 15:31:38 +0900

    Update PlaceDao.kt

--- MatDongYeoJiDo/app/src/main/java/com/jsj/myapplication/data/database/PlaceDao.kt
+++ MatDongYeoJiDo/app/src/main/java/com/jsj/myapplication/data/database/PlaceDao.kt
@@ -1,15 +1,15 @@
-//package com.jsj.myapplication.data.database
-//
-//import androidx.room.Dao
-//import androidx.room.Insert
-//import androidx.room.Query
-//import com.jsj.myapplication.data.model.Place
-//
-//@Dao
-//interface PlaceDao {
-//    @Insert
-//    suspend fun insertPlaces(places: List<Place>)
-//
-//    @Query("SELECT * FROM place")
-//    suspend fun getAllPlaces(): List<Place>
-//}
+package com.jsj.myapplication.data.database
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.jsj.myapplication.data.model.Place
+
+@Dao
+interface PlaceDao {
+   @Insert
+   suspend fun insertPlaces(places: List<Place>)
+
+   @Query("SELECT * FROM place")
+   suspend fun getAllPlaces(): List<Place>
+}
