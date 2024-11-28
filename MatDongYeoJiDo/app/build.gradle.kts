bf300be5f27fe18da7a7254361d0376370918b3d
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2024-11-28 14:23:23 +0900

    feat: 리스트페이지 완료

--- MatDongYeoJiDo/app/build.gradle.kts
+++ MatDongYeoJiDo/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.jsj.myapplication"
-        minSdk = 35
+        minSdk = 34
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
